6649e4a30430f8a5262a4db0be0c6aedac45af0b
Muhidin <muhidinhukic1@gmail.com>
2021-03-08 14:25:59 -0600

    database change

--- db/migrate/20210128001015_add_column_cat_to_notes.rb
+++ db/migrate/20210128001015_add_column_cat_to_notes.rb
@@ -1,5 +1,5 @@
 class AddColumnCatToNotes < ActiveRecord::Migration[6.0]
   def change
-    add_column :notes, :category, :string unless column_exists??
+    add_column :notes, :category, :string
   end
 end
