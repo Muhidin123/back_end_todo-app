dbbde6fe43893acb79ed863795255ac15d531b0d
Muhidin <muhidinhukic1@gmail.com>
2021-03-09 21:17:44 -0600

    auth_controller.rb changed

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,8 +6,8 @@ class AuthController < ApplicationController
       if user && user.authenticate(params[:password])
           payload = { user_id: user.id }
           token = JWT.encode(payload, ENV["MY_SECRET"], 'HS256')
-          all_notes = Note.where(user_id: user.id)
-          render json: { user: {id: user.id, username: user.username, name: user.first_name ,token: token}, notes: all_notes}
+          notes = Note.where(user_id: user.id)
+          render json: { user: {id: user.id, username: user.username, name: user.first_name ,token: token}, notes: notes}
       else
         render json: { error: 'Invalid username or password.' }, status: 401
       end
@@ -21,6 +21,7 @@ class AuthController < ApplicationController
       user = User.find(user_id)
       notes = Note.where(user_id: user.id)
       render json: { user: {id: user.id, username: user.username, name: user.first_name , token: token}, notes: notes}
+
     end
 
 end
